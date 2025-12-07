feaada97d2d634d939f4e1b89ddfd0777b5ef29b
avverma9 <av95766@gmail.com>
2025-12-07 14:55:25 +0530

    FAV

--- server/routes/job.js
+++ server/routes/job.js
@@ -38,7 +38,7 @@ router.get(
 router.get("/get-private-jobs", getPrivateJob); // Fetch Latest Jobs
 // Fetch Latest Jobs
 router.get("/fav-posts", getFavPosts); // Fetch Favorite Posts
-router.put("/mark-fav/:id", markFav); // Mark/Unmark Favorite Post
+router.put("/mark-fav/:id",verifyToken, authorizeRoles("admin", "super_admin"),  markFav); // Mark/Unmark Favorite Post
 router.get("/admit-cards", getAdmitCards); // Fetch Admit Cards
 router.get("/results", getResults); // Fetch Results
 router.get("/exams", getExams); // Fetch Upcoming Exams
