4f167ba82b226fccaabd58b5f3a7bd1e0275fc49
AnvPlayzz@1 <av95766@gmail.com>
2025-12-09 14:46:10 +0530

    done

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,16 +6,19 @@ const {
   markFav,
   getFavPosts,
   getReminders,
-
 } = require("../controller/posts");
 const { verifyToken, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 router.get("/get-sections", getSections);
 router.post("/get-postlist", getPostListBySection);
 router.get("/get-post/details", getPostDetails);
-router.put("/mark-fav/:id",verifyToken, authorizeRoles("admin", "super_admin"),  markFav); // Mark/Unmark Favorite Post
+router.put(
+  "/mark-fav/:id",
+  verifyToken,
+  authorizeRoles("admin", "super_admin"),
+  markFav
+); // Mark/Unmark Favorite Post
 router.get("/fav-posts", getFavPosts); // Fetch Favorite Posts
 router.get("/reminders/expiring-jobs", getReminders);
-
 
 module.exports = router;
