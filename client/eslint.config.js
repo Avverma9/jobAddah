59bb479e8906fa49aec99412c487cbd7024e651b
AnvPlayzz@1 <av95766@gmail.com>
2025-12-22 07:39:49 +0530

    Update ESLint configuration and dependencies; enhance HTML meta tags for SEO

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,18 +1,17 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+// Minimal config that references plugins by name (relies on installed devDependencies)
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
     ],
+    plugins: ['react', 'react-hooks'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -21,6 +20,9 @@ export default defineConfig([
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
+    },
+    settings: {
+      react: { version: 'detect' },
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
